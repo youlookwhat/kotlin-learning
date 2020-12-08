aef3ce3ea76c168917f29ada8fdccb047f61a897
jingbin <770413277@qq.com>
2020-12-08 20:59:48 +0800

    add 重用属性访问的逻辑：委托属性02

--- kotlin/app/src/main/java/com/kotlin/jingbin/kotlinapp/operator/Operator5Activity.kt
+++ kotlin/app/src/main/java/com/kotlin/jingbin/kotlinapp/operator/Operator5Activity.kt
@@ -2,14 +2,11 @@ package com.kotlin.jingbin.kotlinapp.operator
 
 import android.content.Context
 import android.content.Intent
-import android.graphics.Point
-import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.kotlin.jingbin.kotlinapp.R
-import java.lang.reflect.Type
-import java.time.LocalDate
-import kotlin.properties.ReadWriteProperty
+import java.beans.PropertyChangeListener
+import java.beans.PropertyChangeSupport
 
 /**
  * 7.5 重用属性访问的逻辑：委托属性
@@ -29,29 +26,134 @@ class Operator5Activity : AppCompatActivity() {
         /**-------------------- 7.5.1 委托属性的基本操作 ----------------------*/
         // 基础语法：
 //        class Foo {
+        // 关键字 by 把属性关联上委托对象
 //            var p: Type by Delegate()
 //        }
 
         class Foo {
+            // 编译器会自动生成一个辅助属性
 //            private val delegate = Delegate()
+            // p 的访问都会调用对应的 delegate 的getValue和setValue方法
 //            var p: Type
-//                set(value: Type) = delegate.setValue(1, value)
-//                get() = delegate.getValue(1)
+//                set(value: Type) = delegate.setValue(..., value)
+//                get() = delegate.getValue(...)
         }
 
-    }
+        class Delegate {
 
-    class Delegate {
+            // getValue 包含了实现setter的逻辑
+//        fun getValue(...): Type {
+//            ...
+//        }
+
+            // setValue 包含了实现setter的逻辑
 //        fun setValue(x: Int, any: Type) {
-//
+//            ...
 //        }
 //
-//        fun getValue(i: Int): Type {
-//            return i.Ty
-//        }
+        }
 
+//        val foo = Foo()
+        // 通过调用 delegate.getValue(...)来实现属性的修改
+//        val oldValue = foo.p
+        // 通过调用 delegate.setValue(...,newValue)来实现属性的修改
+//        foo.p = newValue
+
+
+        /**-------------------- 7.5.2 使用委托属性：惰性初始化和 by lazy() ----------------------*/
+        // 惰性初始化时一种常见的模式，知道在第一次访问该属性的时候，才根据需要创建对象的一部分。
+
+        class Email
+
+
+        // 使用额外的 _emails 属性来实现惰性加载，在没有加载之前为null，然后加载为邮件列表
+
+        // 代码清单7.17 使用支持属性来实现惰性初始化
+        class Person(val name: String) {
+            // _emails 属性用来保存数据，关联委托
+            private var _emails: List<Email>? = null
+            val emails: List<Email>
+                get() {
+                    if (_emails == null) {
+                        // 访问时加载邮件
+                        _emails = loadEmails(this)
+                    }
+                    //  如果已经加载，就直接返回
+                    return _emails!!
+                }
+
+            fun loadEmails(person: Person): List<Email> {
+                println("Load email for ${person.name}")
+                return listOf()
+            }
+        }
+
+        val p = Person("jingbin")
+        p.emails// 第一次访问会加载邮件: Load email for jingbin
+        p.emails
+        /*
+        * 这里使用了所谓的 支持属性技术。有一个属性 _emails 用来存储这个属性，而另一个email用来提供对属性的读取访问。
+        * 这样代码有点啰嗦，而且线程不安全，kotlin有更好的方案，使用标准库函数 lazy 返回的委托。
+        */
+
+
+        // 代码清单7.18 用委托属性来实现惰性初始化
+        class Person2(val name: String) {
+            // lazy 的参数是一个Lambda，可以调用它来初始化这个值，且默认是线程安全的。
+            val emails by lazy { loadEmail(this) }
+
+            private fun loadEmail(person: Person2): List<Email> {
+                println("Load email2 for ${person.name}")
+                return listOf()
+            }
+        }
+
+
+        /**-------------------- 7.5.3 实现委托属性 ----------------------*/
+        // 代码清单7.19 使用 PropertyChangeSupport 的工具类
+        open class PropertyChangeAware {
+
+            protected val changeSupport = PropertyChangeSupport(this)
+
+            fun addPropertyChangeListener(listener: PropertyChangeListener) {
+                changeSupport.addPropertyChangeListener(listener)
+            }
+
+            fun removePropertyChangeListener(listener: PropertyChangeListener) {
+                changeSupport.removePropertyChangeListener(listener)
+            }
+        }
+
+        // 代码清单7.20 手工实现属性修改的通知
+        class Person3(val name: String, age: Int, salary: Int) : PropertyChangeAware() {
+            var age: Int = age
+                set(newValue) {
+                    // field 标识符允许你访问属性背后的支持字段
+                    val oldValue = field
+                    field = newValue
+                    changeSupport.firePropertyChange("age", oldValue, newValue)
+                }
+
+            var salary: Int = salary
+                set(newValue) {
+                    val oldValue = field
+                    field = newValue
+                    changeSupport.firePropertyChange("salary", oldValue, newValue)
+                }
+        }
+
+        val p2 = Person3("jingbin", 30, 40000)
+        // 关联监听器，用于监听属性修改
+        p2.addPropertyChangeListener(PropertyChangeListener { it ->
+            println("Property ${it.propertyName} changed" + "from ${it.oldValue} to ${it.newValue}")
+        })
+        p2.age = 35 // Property age changed form 30 to 35
+        p2.salary = 60000 // Property salary changed form 40000 to 60000
+
+
+        // 代码清单7.21 提过辅助类来实现属性变化的通知
+        class ObservableProperty(val propName: String, var property: Int) {}
     }
-
 
     companion object {
         fun start(context: Context) {
